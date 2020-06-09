8b4c0a46bb1a3c0f95eb8765871ec73bc419f03a
Kanad Nemade <kanadnemade88@gmail.com>
2020-06-09 13:26:19 +0530

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -1,5 +1,3 @@
-pip install colorama selenium mechanize pygoogling
-easy_install hashlib
-
-# optparse, smtplib and platform  modules are already installed in pyhton2.7
-# by default so it will be there when you install(update) python2
+pip3 install colorama selenium mechanize pygoogling
+# optparse,haslib, smtplib and platform  modules are already installed in pyhton3
+# by default so it will be there when you install(update) python3
